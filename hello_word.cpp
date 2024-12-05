b9fa0a3491c25a92e23e24cf61046981ca978099
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-12-05 21:15:15 +0300

    Create hello word

---
+++ hello_word.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Введите имя пользоваетеля:";
+    cin>>name;
+    cout<<"Hello word from"<<name<<endl;
+    return 0;
+}
