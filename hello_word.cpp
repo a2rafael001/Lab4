225faf75d92fb58e873b6836afdd6297ccba2572
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-12-07 13:24:00 +0300

    Edit comment

--- hello_word.cpp
+++ hello_word.cpp
@@ -2,9 +2,9 @@
 // Delete using namespace std; 
 
 int main(){
-    std::string name;
-    std:: cout<<"Введите имя пользоваетеля:";
-    std::cin>>name;
-    std:: cout<<"Hello word from"<<name<<std::endl;
+    std::string name;//Переменная,используем для хранения имени пользователя
+    std:: cout<<"Введите имя пользоваетеля:";//вывод на консоль сообщения
+    std::cin>>name;//ввод имя пользователя
+    std:: cout<<"Hello word from"<<name<<std::endl;//вывод сообщения
     return 0;
 }
