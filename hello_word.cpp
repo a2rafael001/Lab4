c879ef3b3e35a6f0b39574f98a3ce51f55628c07
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-12-07 14:51:40 +0300

    Edit comment2

--- hello_word.cpp
+++ hello_word.cpp
@@ -2,10 +2,11 @@
 // Delete using namespace std; 
 
 int main(){
+   
     std::string name;//Переменная,используем для хранения имени пользователя
     std:: cout<<"Введите имя пользоваетеля:";//requesting keyboard input
     std::cin>>name;//keyboard input
     std:: cout<<"Hello word from"<<name<<std::endl;//вывод сообщения
     return 0;
-  
+   
 }
