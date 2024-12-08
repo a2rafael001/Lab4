7af1a938f316a6d42d0d94bc8cf346301046f89f
a2rafael001 <rafael.arutyunyan.74@mail.ru>
2024-12-09 01:38:06 +0300

    Edit commemt.

--- hello_word.cpp
+++ hello_word.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-// Dellete using namespace std; 
-
-int main(){
-   
-    std::string name;//Переменная,используем для хранения имени пользователя
-    std:: cout<<"Введите имя пользоваетеля:";//requesting keyboard input
-    std::cin>>name;//keyboard input
-    std:: cout<<"Hello word from"<<name<<std::endl;//вывод сообщения
+//Удалили using namespace std;
+int main() {
+<<<<<<< HEAD
+    string name; //Переменная, используемая для хранения имени пользователя
+    std::cout<<"Введите имя пользователя:";//Requesting keyboard input
+    std::cin>>name;//Keyboard input
+=======
+    string name; //Переменная для хранения имени пользователя
+    std::cout<<"Введите имя пользователя:";//Просьба ввода с клавиатуры
+    std::cin>>name;//Принятие ввода
+>>>>>>> 8b61e21557304c91bd23ff80a0ee76d21d4ab410
+    std::cout << "Hello world from "<<name<<std::endl;//Вывод 123
     return 0;
-   
 }
